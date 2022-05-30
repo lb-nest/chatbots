6bba11fefd6dab49450e778a8a0881f176a099b5
xanedesu <xanedesu@gmail.com>
2022-05-30 22:36:40 +0300

    Добавил свойство trigger в токен

--- src/shared/services/chatbots-token.provider.ts
+++ src/shared/services/chatbots-token.provider.ts
@@ -1,19 +1,21 @@
 import { Injectable } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
 import jwt from 'jsonwebtoken';
+import { TriggerType } from '../models';
 
 @Injectable()
 export class ChatbotsTokenProvider {
   constructor(private readonly configService: ConfigService) {}
 
-  get(id: number, projectId: number): string {
+  get(id: number, projectId: number, trigger: TriggerType): string {
     return jwt.sign(
       {
         id,
-        ws: this.configService.get<string>('BACKEND_URL').replace('http', 'ws'),
+        ws: this.configService.get<string>('CHATBOTS_EDGE_URL'),
         project: {
           id: projectId,
         },
+        trigger,
       },
       this.configService.get<string>('SECRET'),
       {
