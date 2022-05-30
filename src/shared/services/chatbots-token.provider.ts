8543eccbd2ea960099eae8b877deb4e661db005d
xanedesu <xanedesu@gmail.com>
2022-05-30 22:38:19 +0300

    Удалил trigger

--- src/shared/services/chatbots-token.provider.ts
+++ src/shared/services/chatbots-token.provider.ts
@@ -1,13 +1,12 @@
 import { Injectable } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
 import jwt from 'jsonwebtoken';
-import { TriggerType } from '../models';
 
 @Injectable()
 export class ChatbotsTokenProvider {
   constructor(private readonly configService: ConfigService) {}
 
-  get(id: number, projectId: number, trigger: TriggerType): string {
+  get(id: number, projectId: number): string {
     return jwt.sign(
       {
         id,
@@ -15,7 +14,6 @@ export class ChatbotsTokenProvider {
         project: {
           id: projectId,
         },
-        trigger,
       },
       this.configService.get<string>('SECRET'),
       {
