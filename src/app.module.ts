a8b18d02bf4c07dd43fea4d350903ba99f5007f4
xanedesu <xanedesu@gmail.com>
2022-06-21 16:11:52 +0300

    Валидация переменных окружения

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,13 +1,23 @@
 import { ClassSerializerInterceptor, Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { APP_INTERCEPTOR } from '@nestjs/core';
+import Joi from 'joi';
 import { AuthModule } from './auth/auth.module';
 import { ChatbotTemplateModule } from './chatbot-template/chatbot-template.module';
 import { ChatbotModule } from './chatbot/chatbot.module';
 
 @Module({
   imports: [
-    ConfigModule.forRoot(),
+    ConfigModule.forRoot({
+      validationSchema: Joi.object({
+        DATABASE_URL: Joi.string().uri().required(),
+        PORT: Joi.number().default(8080),
+        SECRET: Joi.string().required(),
+        AUTHORIZATION_URL: Joi.string().uri().required(),
+        CHATBOTS_EDGE_URL: Joi.string().uri().required(),
+        CHATBOTS_CONTAINER_URLS: Joi.string().required(),
+      }),
+    }),
     AuthModule,
     ChatbotModule,
     ChatbotTemplateModule,
